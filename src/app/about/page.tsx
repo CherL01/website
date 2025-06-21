b6c933e0e78657ae2afcee316535f5a914bf46e1
Cherry Lian <cherrylian01@gmail.com>
2025-06-21 16:13:42 -0700

    changed About page timeline animation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -79,8 +79,8 @@ export default function AboutPage() {
                 <motion.div
                   key={`${item.type}-${index}`}
                   className="relative flex items-start"
-                  initial={{ opacity: 0, x: -50 }}
-                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.6, delay: index * 0.1 }}
                   viewport={{ once: true }}
                 >
